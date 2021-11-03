4a20b9d7e80ce90f1b8e8a361fb482619a8135eb
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-03 13:30:05 +0300

    api

--- src/components/Carid/index.js
+++ src/components/Carid/index.js
@@ -1,6 +1,7 @@
 
 
-import {React, useState} from 'react'
+import axios from 'axios';
+import {React} from 'react'
 import { useParams} from "react-router";
 import "./stayle.css"
 
@@ -8,31 +9,6 @@ import "./stayle.css"
 
 const Carid = () => {
   
-   
-    const [card] = useState([
-        {
-          id: 1,
-          City: "Kingdom Saudi Arabia",
-          imgurl:"https://images.pexels.com/photos/5561857/pexels-photo-5561857.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        },
-        {
-          id: 2,
-          City: "United States of America",
-          imgurl:"https://images.pexels.com/photos/5847357/pexels-photo-5847357.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        },
-    
-        {
-          id: 3,
-          City: "France",
-          imgurl:"https://images.pexels.com/photos/739407/pexels-photo-739407.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ",
-        },
-      ])
-    
-    const {id} = useParams();
-    
-    let myId = card.find((item)=>{
-        return(Number(id)===item.id)
-    })
     
    
     return (
@@ -40,8 +16,7 @@ const Carid = () => {
         <div className="ciald-image">
                  
 
-            <h1>{myId.City}</h1>
-            <img src={myId.imgurl}/>
+            <h1>heloo</h1>
         </div>
     )
     
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import {React,useState, useEffect} from "react";
 import "./stayle.css";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 
 
@@ -9,24 +10,18 @@ const Cards = () => {
     const history = useHistory()
    
     
-  const card = [
-    {
-      id: 1,
-      City: "Kingdom Saudi Arabia",
-      imgurl:"https://images.pexels.com/photos/5561857/pexels-photo-5561857.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    },
-    {
-      id: 2,
-      City: "United States of America",
-      imgurl:"https://images.pexels.com/photos/5847357/pexels-photo-5847357.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    },
+    const [pok,setPok]=useState([]);
 
-    {
-      id: 3,
-      City: "France",
-      imgurl:"https://images.pexels.com/photos/739407/pexels-photo-739407.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ",
-    },
-  ];
+    useEffect(() => {
+      getpok();
+     
+    }, [])
+
+  
+ const getpok = async ()=>{
+   const respinciv = await axios.get('https://api.pokemontcg.io/v2/cards?pageSize=3');
+   setPok(respinciv.data.data)
+ }
 
   const cihing =(id)=>{
   history.push(`/Carid/${id}`)
@@ -35,13 +30,13 @@ console.log(id);
 
   return (
     <div className="pearntimage">
-      <h1>cards</h1>
-      {card.map((item) => {
+      
+      {pok.map((item) => {
         return (
             
           <div className="cardimage" >
-            <h1>{item.City}</h1>
-            <img src={item.imgurl} onClick={() => cihing(item.id)} />
+            <h1>{item.name}</h1>
+            <img src={item.images.small} onClick={() => cihing(item.id)} />
           </div>
         );
       })}
